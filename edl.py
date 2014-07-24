339901ce89b20e0ab28b1f969e0526454e1a44a0
Red5d <daniel.colson99@gmail.com>
2014-07-23 23:40:58 -0400

    Added my edl.py EDL processing module.

---
+++ edl.py
@@ -0,0 +1,44 @@
+class reader(object):
+
+    def __init__(self, edlfile):
+        self.edlfile = edlfile
+
+    def read(self):
+        print self.edlfile.read()
+
+
+class struct(object):
+
+    def __init__(self, edlfile):
+        self.edlfile = edlfile
+        self.time1 = list()
+        self.time2 = list()
+        self.action = list()
+
+        for line in self.edlfile:
+             if len(line.split('      ')) == 3:
+                 self.time1.append(line.split("      ")[0])
+                 self.time2.append(line.split("      ")[1])
+                 self.action.append(line.split("      ")[2].split('\n')[0])
+
+
+class writer(object):
+
+    def __init__(self, edlfile):
+        self.edlfile = edlfile
+
+    def writeline(self, time1, time2, action):
+        self.time1 = time1
+        self.time2 = time2
+        self.action = action
+        
+        self.edlfile.write(str(time1)+"      "+str(time2)+"      "+str(action))
+        
+    def write_struct(self, estruct):
+        self.time1 = estruct.time1
+        self.time2 = estruct.time2
+        self.action = estruct.action
+        for x in range(0, len(self.time1)):
+            self.edlfile.write(str(self.time1[x])+"      "+str(self.time2[x])+"      "+str(self.action[x])+"\n")
+        
+        
