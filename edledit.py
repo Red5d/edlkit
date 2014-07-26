38a1a34b0011ba340decc2baf5d0e93dd14534f5
Red5d <daniel.colson99@gmail.com>
2014-07-26 16:38:24 -0400

    Fixed bug where the last section of video after an edit got cut entirely.

--- edledit.py
+++ edledit.py
@@ -13,23 +13,33 @@ for line in row:
     info = line.split()
     nextTime = float(info[0])
     time2 = float(info[1])
-    action = int(info[2])
+    action = info[2]
 
     clip = VideoFileClip(sys.argv[1]).subclip(prevTime,nextTime)
     clips = concatenate([clips,clip])
     print "created subclip from " + str(prevTime) + " to " + str(nextTime)
 
+    global prevTime
     prevTime = nextTime
     nextTime = time2
 
-    if action == 1:
+    if action == "1":
         clip = VideoFileClip(sys.argv[1], audio = False).subclip(prevTime,nextTime)
         clips = concatenate([clips,clip])
         print "created muted subclip from " + str(prevTime) + " to " + str(nextTime)
-    else:
+    elif action == "0":
         print "Muted video." #do nothing (video muted)
+    else:
+        clip = VideoFileClip(sys.argv[1]).subclip(prevTime,nextTime)
+        clips = concatenate([clips,clip])
         
+    global prevTime
     prevTime = nextTime
 
+
+videoLength = VideoFileClip(sys.argv[1]).duration
+clip = VideoFileClip(sys.argv[1]).subclip(prevTime,videoLength)
+print "created ending clip from " + str(prevTime) + " to " + str(videoLength)
+clips = concatenate([clips,clip])
 clips.to_videofile(sys.argv[3])
 
