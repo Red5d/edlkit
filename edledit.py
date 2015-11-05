45689813bf0cb64196a12f03e69aeb36f3160168
Red5d <daniel.colson99@gmail.com>
2015-11-04 20:31:09 -0500

    Fixed bitrate setting so the resulting video will have the same (or similar) bitrate as the original. Also added options for using an ffmpeg preset and specifying the number of threads to use. Included usage info for the additional options in the Usage that displays when the script is run without enough parameters.

--- edledit.py
+++ edledit.py
@@ -2,12 +2,28 @@ from moviepy.editor import *
 import sys
 
 if len(sys.argv) < 3:
-    print(str('Usage: python edledit.py "Video File.mp4" "EDL File.edl" "Edited Video File.mp4"'))
+    print(str('Usage: python edledit.py "Video File.mp4" "EDL File.edl" "Edited Video File.mp4" [threads] [preset]'))
+    print(str(''))
+    print(str('Threads: Optional. A number between 1 and the number of processor cores you have. Defaults to 2.'))
+    print(str('Preset:  Optional. Changes how well-optimized the compression is. Affects speed and file size, not quality.'))
+    print(str('         Choices: ultrafast, superfast, fast, medium, slow, superslow. Defaults to "medium".'))
+    print(str(''))
     exit()
 
 
 file = open(sys.argv[2], 'r')
 row = file.readlines()
+
+if len(sys.argv) < 4:
+    threadNum = 2
+else:
+    threadNum = sys.argv[4]
+
+if len(sys.argv) < 5:
+    ffmpegPreset = "medium"
+else:
+    ffmpegPreset = sys.argv[5]
+
 
 clipNum = 1
 global prevTime
@@ -45,5 +61,5 @@ videoLength = VideoFileClip(sys.argv[1]).duration
 clip = VideoFileClip(sys.argv[1]).subclip(prevTime,videoLength)
 print("created ending clip from " + str(prevTime) + " to " + str(videoLength))
 clips = concatenate([clips,clip])
-clips.write_videofile(sys.argv[3], fps=24, codec="libx264", threads=2)
+clips.write_videofile(sys.argv[3], fps=24, bitrate="50000k", preset=ffmpegPreset, codec="libx264", threads=threadNum)
 
