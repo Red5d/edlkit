462284a0c4a02e8aa1e9a80118350d6189fa10d7
Red5d <daniel.colson99@gmail.com>
2016-01-10 00:13:20 -0500

    Added more package info to setup.py, fixed it not including scripts in packages built from it, and fixed the edledit script definition to enter the 'main' function.

--- setup.py
+++ setup.py
@@ -2,11 +2,18 @@ from setuptools import setup, find_packages
 setup(
     name = "EDLKit",
     version = "1.0",
+    description = "Python tools to create, tweak, and apply EDL file edits to video files.",
+    url = "https://github.com/Red5d/edlkit",
+    author = "Red5d",
+    license = "GPLv2",
+    keywords = "video edl ffmpeg moviepy edit editing",
+    
     packages = find_packages(),
     install_requires = ['moviepy>=0.2.2'],
+    scripts = ['edledit.py', 'edlcreate.py', 'edlcreate_timer.py', 'edltweak_gui.py'],
     entry_points={
         'console_scripts': [
-            'edledit = edledit',
+            'edledit = edledit:main',
             'edlcreate = edlcreate',
             'edlcreate_timer = edlcreate_timer',
         ],
